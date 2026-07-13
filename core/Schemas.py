b0978742428e22823f0a17968c0201d0e9a3b952
Mehdi <wishtree1248@gmail.com>
2026-07-13 20:38:15 +0330

    Starting to work with Validation in pydantic

--- core/Schemas.py
+++ core/Schemas.py
@@ -1,7 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,field_validator
+import re
 
 class BasePersonSchema (BaseModel):
     name: str
+
+    @field_validator("name")
+    def validate_name(cls, value):
+        if len(value) > 33:
+            raise ValueError("Name must not exceed 33 characters")
+        pattern = r'^[a-zA-Z\s]+$'
+        if not bool(re.match(pattern, value)):
+            raise ValueError("Name must only contain alphabetic characters and space")
+        return value
 
 class PersonCreateSchema (BasePersonSchema):
     pass
