3785f7b4d3c9ff3ccfce9767b7838061ca096d14
Mehdi <wishtree1248@gmail.com>
2026-07-14 20:46:13 +0330

    Starting to work and test Serialization in pydantic

---
+++ core/test_serialization.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr, field_serializer
+
+class User(BaseModel):
+    name: str
+    email: EmailStr
+    account_id: int
+
+class Model(BaseModel):
+    number: float
+    @field_serializer("number")
+    def serializ_float(self, value):
+        return round(value, 5)
+
+
+user = User(name = "Mehdi", email = "Ostad@gmail.com", account_id= "123")
+
+A = user.model_dump()
+print (A)
+
+B = user.model_dump_json(indent = 5)
+print (B)
+
+C = Model (number = 2/3)
+print (C.model_dump())
